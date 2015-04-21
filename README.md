5259796c73cb07a3eb0e1a47b42da6bab59efff4
Derrick Showers <dshowers@linkedin.com>
2015-04-20 18:19:51 -0700

    Initial Commit - README, Apple boilerplate, gitignore

---
+++ README.md
@@ -0,0 +1,5 @@
+# Recommend-It (Version 2.0)
+
+If you're looking for the version that's currently on the App Store, go here (or check out the repo). This is a complete rewrite from the ground up, using the latest versio of Swift (currently 1.2).
+
+More details to come...
---
+++ Recommend-It/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Recommend-It
+//
+//  Created by Derrick Showers on 4/20/15.
+//  Copyright (c) 2015 Derrick Showers. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
