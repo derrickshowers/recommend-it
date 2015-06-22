2fcc2cd540dcb1be4e2ab70de0e88568d58bfcd2
Derrick Showers <dshowers@linkedin.com>
2015-06-21 17:28:46 -0700

    subclassed UICollectionViewReusableView for header view

---
+++ Recommend-It/Views/FeedHeaderReusableView.swift
@@ -0,0 +1,13 @@
+//
+//  FeedHeaderCell.swift
+//  Recommend-It
+//
+//  Created by Derrick Showers on 6/21/15.
+//  Copyright (c) 2015 Derrick Showers. All rights reserved.
+//
+
+import UIKit
+
+class FeedHeaderReusableView: UICollectionReusableView {
+    @IBOutlet weak var feedHeaderImage: UIImageView!
+}
