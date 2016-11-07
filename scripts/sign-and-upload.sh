d0543dbae96f453361cf0ffd249641119cb34d95
Derrick Showers <derrick@derrickshowers.com>
2016-11-06 17:52:41 -0800

    Oh, only the name of the provisioning profile

--- scripts/sign-and-upload.sh
+++ scripts/sign-and-upload.sh
@@ -17,7 +17,7 @@ gem install shenzhen
 
 # Archive and package as IPA
 xcodebuild archive -workspace Recommend-It.xcworkspace -scheme Recommend-It -sdk iphoneos -archivePath ./build/Recommend-It.xcarchive
-xcodebuild -exportArchive -archivePath ./build/Recommend-It.xcarchive -exportPath ./build/Recommend-It.ipa -exportProvisioningProfile $PROFILE_NAME.mobileprovision
+xcodebuild -exportArchive -archivePath ./build/Recommend-It.xcarchive -exportPath ./build/Recommend-It.ipa -exportProvisioningProfile $PROFILE_NAME
 
 # Send to iTunes connect
 ipa distribute:itunesconnect -a derrick.showers@me.com -p $ITUNES_PASSWORD -i 951556200 -f ./build/Recommend-It.ipa --upload --verbose
