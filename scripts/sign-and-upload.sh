374a46131957918ef19d512b004d9599219f1f46
Derrick Showers <derrick@derrickshowers.com>
2016-11-06 18:04:03 -0800

    Use UDID for provisioning profile identifier

--- scripts/sign-and-upload.sh
+++ scripts/sign-and-upload.sh
@@ -17,7 +17,7 @@ gem install shenzhen
 
 # Archive and package as IPA
 xcodebuild archive -workspace Recommend-It.xcworkspace -scheme Recommend-It -sdk iphoneos -archivePath ./build/Recommend-It.xcarchive
-xcodebuild -exportArchive -archivePath ./build/Recommend-It.xcarchive -exportPath ./build/Recommend-It.ipa -exportProvisioningProfile $PROFILE_NAME
+xcodebuild -exportArchive -archivePath ./build/Recommend-It.xcarchive -exportPath ./build/Recommend-It.ipa -exportProvisioningProfile fcc95842-32a3-474d-a951-e21cb4c95158
 
 # Send to iTunes connect
 ipa distribute:itunesconnect -a derrick.showers@me.com -p $ITUNES_PASSWORD -i 951556200 -f ./build/Recommend-It.ipa --upload --verbose
