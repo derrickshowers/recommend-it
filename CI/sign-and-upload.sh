f1851cc3c88dbbd3d4c9b3ce138f0e5c787dbf3a
Derrick Showers <derrick.showers@gmail.com>
2016-11-06 20:25:38 -0800

    Enable Travis for testing and deployment (#17)

---
+++ CI/sign-and-upload.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+  echo "This is a pull request. No deployment will be done."
+  exit 0
+fi
+if [[ "$TRAVIS_BRANCH" != "master" ]]; then
+  echo "Testing on a branch other than master. No deployment will be done."
+  exit 0
+fi
+
+# Install shenzhen for `ipa distribute`
+gem install shenzhen
+
+# Archive and package as IPA
+xcodebuild -workspace Recommend-It.xcworkspace -scheme Recommend-It -sdk iphoneos -configuration AppStoreDistribution archive -archivePath ./build/Recommend-It.xcarchive
+xcodebuild -exportArchive -archivePath ./build/Recommend-It.xcarchive -exportOptionsPlist ./CI/config/exportOptions.plist -exportPath ./build
+
+# Send to iTunes connect
+ipa distribute:itunesconnect -a derrick.showers@me.com -p $ITUNES_PASSWORD -i 951556200 -f ./build/Recommend-It.ipa --upload --verbose
