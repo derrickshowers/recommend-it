553d7422a0d516003ff2eb846086a0d7f33a711f
Derrick Showers <derrick@derrickshowers.com>
2016-11-27 10:20:44 -0800

    Added command to remove UI prompt w/keychain

--- CI/sign-and-upload.sh
+++ CI/sign-and-upload.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
-  echo "This is a pull request. No deployment will be done."
-  exit 0
-fi
-if [[ "$TRAVIS_BRANCH" != "master" ]]; then
-  echo "Testing on a branch other than master. No deployment will be done."
-  exit 0
-fi
+# if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+#   echo "This is a pull request. No deployment will be done."
+#   exit 0
+# fi
+# if [[ "$TRAVIS_BRANCH" != "master" ]]; then
+#   echo "Testing on a branch other than master. No deployment will be done."
+#   exit 0
+# fi
 
 # Install shenzhen for `ipa distribute`
 gem install shenzhen
