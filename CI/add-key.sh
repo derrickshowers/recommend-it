72e8922178cb9ea5fef6a6cc1333ff0a949a11e6
Derrick Showers <derrick.showers@gmail.com>
2017-04-02 12:36:39 -0700

    Xcode 8.3 (#42)
    * Upgrade Travis to 8.3 & minor tweaks to scripts
    * Remove all development certs and profiles

--- CI/add-key.sh
+++ CI/add-key.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 KEY_CHAIN=ios-build.keychain
+CERTS_DIR=./CI/certs/
+PROFILE_DIR=./CI/profile/
+
+# Decrypt certs and provisioning profiles
+openssl aes-256-cbc -k "$ENCRYPTION_SECRET_PROVISIONING_PROFILE" -in ${PROFILE_DIR}RecommendIt_Distribution.enc -d -a -out ${PROFILE_DIR}RecommendIt_Distribution.mobileprovision
+openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in ${CERTS_DIR}distribution.p12.enc -d -a -out ${CERTS_DIR}distribution.p12
 
 # Create a custom keychain
 security create-keychain -p travis $KEY_CHAIN
@@ -16,7 +22,6 @@ security unlock-keychain -p travis $KEY_CHAIN
 security set-keychain-settings -t 3600 -u $KEY_CHAIN
 
 # Add certificates to keychain and allow codesign to access them
-security import ./CI/certs/development.p12 -k $KEY_CHAIN -P $KEY_PASSWORD -T /usr/bin/codesign
 security import ./CI/certs/distribution.p12 -k $KEY_CHAIN -P $KEY_PASSWORD -T /usr/bin/codesign
 
 # Remove UI prompt causing build to timeout
@@ -25,5 +30,4 @@ security set-key-partition-list -S apple-tool:,apple: -s -k travis $KEY_CHAIN
 
 # Put the provisioning profile in place
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
-cp "./CI/profile/XC_iOS_comderrickshowersRecommendIt_Development.mobileprovision" ~/Library/MobileDevice/Provisioning\ Profiles/
-cp "./CI/profile/XC_iOS_comderrickshowersRecommendIt_Distribution.mobileprovision" ~/Library/MobileDevice/Provisioning\ Profiles/
+cp "${PROFILE_DIR}RecommendIt_Distribution.mobileprovision" ~/Library/MobileDevice/Provisioning\ Profiles/
